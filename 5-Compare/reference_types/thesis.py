1b9af27d0262d21be9be10f2e8ad3a81487fc3f8
Engn21 <117390683+Engn21@users.noreply.github.com>
2025-11-22 11:48:14 +0100

    Add Thesis class with fields and weights

--- 5-Compare/reference_types/thesis.py
+++ 5-Compare/reference_types/thesis.py
@@ -1,1 +1,23 @@
+class Thesis:
+    FIELDS = [
+        "title", "authors", "year",
+        "institution", "thesis_type", "department",
+        "advisors", "pages", "location"
+    ]
+
+    WEIGHTS = {
+        "title":        0.50,
+        "authors":      0.40,
+        "year":         0.10,
+        "institution":  0.35,
+        "thesis_type":  0.25,
+        "department":   0.15,
+        "advisors":     0.10,
+        "pages":        0.05,
+        "location":     0.05
+    }
+
+    def __init__(self, **fields):
+        self.type = "thesis"
+        self.fields = fields
 
